c81016fc985f8276ce102d63073907a407438420
HERMAN FRIAS <hermanfrias@hotmail.com>
2025-12-09 22:04:47 -0400

    modificacion de listado de cita y calendario para que no aparesca la fecha de entrega si fue borrada

--- citas/forms.py
+++ citas/forms.py
@@ -20,6 +20,14 @@ class CitaForm(forms.ModelForm):
             'required': False  # Evitar validación HTML5
         })
     )
+    fecha_entrega = forms.DateField(
+        required=False,
+        widget=forms.DateInput(attrs={
+            'type':'date',
+            'class':'form-control',
+            'required': False  # Evitar validación HTML5
+        }, format='%Y-%m-%d')
+    )
     
     class Meta:
         model = Cita
@@ -29,7 +37,6 @@ class CitaForm(forms.ModelForm):
         widgets = {
             'cliente': forms.TextInput(attrs={'class':'form-control'}),
             'telefono': forms.TextInput(attrs={'class':'form-control'}),
-            'fecha_entrega': forms.DateInput(attrs={'type':'date','class':'form-control'}, format ='%Y-%m-%d'),
             'descripcion': forms.Textarea(attrs={'class':'form-control','rows':3}),
             'copa': forms.TextInput(attrs={'class':'form-control'}),
             'busto': forms.TextInput(attrs={'class':'form-control'}),
